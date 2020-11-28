07eb7afd37ed7d3f24abf9c27177476b4466f302
Chelsea Garcia <72681344+chelseagarcia0@users.noreply.github.com>
2020-11-29 00:07:11 +0800

    Update PlayState.lua
    Line 31(PlayState:update(dt)) to 133(PlayState:render())

--- fifty bird source code/states/PlayState.lua
+++ fifty bird source code/states/PlayState.lua
@@ -28,77 +28,92 @@ function PlayState:init()
 end
 
 function PlayState:update(dt)
-    -- update timer for pipe spawning
-    self.timer = self.timer + dt
+    if scrolling == true then
+        -- update timer for pipe spawning
+        self.timer = self.timer + dt
 
-    -- spawn a new pipe pair every second and a half
-    if self.timer > 2 then
-        -- modify the last Y coordinate we placed so pipe gaps aren't too far apart
-        -- no higher than 10 pixels below the top edge of the screen,
-        -- and no lower than a gap length (90 pixels) from the bottom
-        local y = math.max(-PIPE_HEIGHT + 10, 
-            math.min(self.lastY + math.random(-20, 20), VIRTUAL_HEIGHT - 90 - PIPE_HEIGHT))
-        self.lastY = y
+        -- spawn a new pipe pair every second and a half
+        if self.timer > math.random(2, 50) then
+            -- modify the last Y coordinate we placed so pipe gaps aren't too far apart
+            -- no higher than 20 pixels below the top edge of the screen,
+            -- and no lower than a gap length (50 pixels) from the bottom
+            local y = math.max(-PIPE_HEIGHT + 20, math.min(self.lastY + math.random(-20, 20), (VIRTUAL_HEIGHT - 80 - PIPE_HEIGHT)))
+            self.lastY = y
 
-        -- add a new pipe pair at the end of the screen at our new Y
-        table.insert(self.pipePairs, PipePair(y))
+            -- add a new pipe pair at the end of the screen at our new Y
+            table.insert(self.pipePairs, PipePair(y))
 
-        -- reset timer
-        self.timer = 0
-    end
+            -- reset timer
+            self.timer = 0
+        end
 
-    -- for every pair of pipes..
-    for k, pair in pairs(self.pipePairs) do
-        -- score a point if the pipe has gone past the bird to the left all the way
-        -- be sure to ignore it if it's already been scored
-        if not pair.scored then
-            if pair.x + PIPE_WIDTH < self.bird.x then
-                self.score = self.score + 1
-                pair.scored = true
-                sounds['score']:play()
+        -- for every pair of pipes..
+        for k, pair in pairs(self.pipePairs) do
+            -- score a point if the pipe has gone past the bird to the left all the way
+            -- be sure to ignore it if it's already been scored
+            if not pair.scored then
+                if pair.x + PIPE_WIDTH < self.bird.x then
+                    self.score = self.score + 1
+                    pair.scored = true
+                    sounds['score']:play()
+                end
+            end
+
+            -- update position of pair
+            pair:update(dt)
+        end
+
+        -- we need this second loop, rather than deleting in the previous loop, because
+        -- modifying the table in-place without explicit keys will result in skipping the
+        -- next pipe, since all implicit keys (numerical indices) are automatically shifted
+        -- down after a table removal
+        for k, pair in pairs(self.pipePairs) do
+            if pair.remove then
+                table.remove(self.pipePairs, k)
             end
         end
 
-        -- update position of pair
-        pair:update(dt)
-    end
+        -- simple collision between bird and all pipes in pairs
+        for k, pair in pairs(self.pipePairs) do
+            for l, pipe in pairs(pair.pipes) do
+                if self.bird:collides(pipe) then
+                    sounds['explosion']:play()
+                    sounds['hurt']:play()
 
-    -- we need this second loop, rather than deleting in the previous loop, because
-    -- modifying the table in-place without explicit keys will result in skipping the
-    -- next pipe, since all implicit keys (numerical indices) are automatically shifted
-    -- down after a table removal
-    for k, pair in pairs(self.pipePairs) do
-        if pair.remove then
-            table.remove(self.pipePairs, k)
-        end
-    end
-
-    -- simple collision between bird and all pipes in pairs
-    for k, pair in pairs(self.pipePairs) do
-        for l, pipe in pairs(pair.pipes) do
-            if self.bird:collides(pipe) then
-                sounds['explosion']:play()
-                sounds['hurt']:play()
-
-                gStateMachine:change('score', {
-                    score = self.score
-                })
+                    gStateMachine:change('score', {
+                        score = self.score
+                    })
+                end
             end
         end
+
+        -- update bird based on gravity and input
+        self.bird:update(dt)
+
+        -- reset if we get to the ground
+        if self.bird.y > VIRTUAL_HEIGHT - 15 then
+            sounds['explosion']:play()
+            sounds['hurt']:play()
+
+            gStateMachine:change('score', {
+                score = self.score
+            })
+        end
+    end    
+
+    -- to pause or play the game
+    if love.keyboard.wasPressed('p') then
+        if scrolling == true then
+            scrolling = false
+            sounds['pause']:play()
+            sounds['music']:pause()
+        elseif scrolling == false then
+            scrolling = true
+            sounds['pause']:play()
+            sounds['music']:play()
+        end
     end
-
-    -- update bird based on gravity and input
-    self.bird:update(dt)
-
-    -- reset if we get to the ground
-    if self.bird.y > VIRTUAL_HEIGHT - 15 then
-        sounds['explosion']:play()
-        sounds['hurt']:play()
-
-        gStateMachine:change('score', {
-            score = self.score
-        })
-    end
+    
 end
 
 function PlayState:render()
@@ -110,6 +125,11 @@ function PlayState:render()
     love.graphics.print('Score: ' .. tostring(self.score), 8, 8)
 
     self.bird:render()
+
+    if scrolling == false then
+        love.graphics.setFont(flappyFont)
+        love.graphics.printf('Paused', 0, 110, VIRTUAL_WIDTH, 'center')
+    end
 end
 
 --[[
