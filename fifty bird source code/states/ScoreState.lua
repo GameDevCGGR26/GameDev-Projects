26b862029b3e1ee81429d9315542065de8ee5c6b
DanielRanay <72848844+DanielRanay@users.noreply.github.com>
2020-11-28 15:48:15 +0800

    Update Score.lua all
    I update all parts in this state

--- fifty bird source code/states/ScoreState.lua
+++ fifty bird source code/states/ScoreState.lua
@@ -16,6 +16,12 @@ ScoreState = Class{__includes = BaseState}
 ]]
 function ScoreState:enter(params)
     self.score = params.score
+
+    self.medals = {
+        ['gold'] = love.graphics.newImage('gold.png'),
+        ['silver'] = love.graphics.newImage('silver.png'),
+        ['bronze'] = love.graphics.newImage('bronze.png')
+    }
 end
 
 function ScoreState:update(dt)
@@ -27,11 +33,28 @@ end
 
 function ScoreState:render()
     -- simply render the score to the middle of the screen
-    love.graphics.setFont(flappyFont)
-    love.graphics.printf('Oof! You lost!', 0, 64, VIRTUAL_WIDTH, 'center')
 
     love.graphics.setFont(mediumFont)
-    love.graphics.printf('Score: ' .. tostring(self.score), 0, 100, VIRTUAL_WIDTH, 'center')
+    love.graphics.printf('Score: ' .. tostring(self.score), 0, 160, VIRTUAL_WIDTH, 'center')
 
-    love.graphics.printf('Press Enter to Play Again!', 0, 160, VIRTUAL_WIDTH, 'center')
+    -- GOLD MEDAL -> 50 OR higher
+    if self.score >= 50 then
+        love.graphics.draw(self.medals['gold'], 206, 180)
+        love.graphics.setFont(flappyFont)
+        love.graphics.printf('Congratulations Birdie!', 0, 64, VIRTUAL_WIDTH, 'center')
+    --SILVER MEDAL -> 25 OR More than 25 Points
+    elseif self.score >= 25 then
+        love.graphics.draw(self.medals['silver'], 206, 25)
+        love.graphics.setFont(flappyFont)
+        love.graphics.printf('Too close to Gold, Birdie!', 0, 64, VIRTUAL_WIDTH, 'center')
+    --BRONZE MEDAL -> 10 OR More than 10 Points
+    elseif self.score >= 1 then
+        love.graphics.draw(self.medals['bronze'], 206, 25)love.graphics.setFont(flappyFont)
+        love.graphics.printf('Keep up!', 0, 120, VIRTUAL_WIDTH, 'center')
+    else
+        love.graphics.setFont(flappyFont)
+        love.graphics.printf('Oof! You lose!', 0, 120, VIRTUAL_WIDTH, 'center')
+    end
+
+    love.graphics.printf('Press Enter to Play Again!', 0, 230, VIRTUAL_WIDTH, 'center')
 end
