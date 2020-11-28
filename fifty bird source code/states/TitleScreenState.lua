67a1705833d0df0137d439cce53739ac3677e1ca
Chelsea Garcia <72681344+chelseagarcia0@users.noreply.github.com>
2020-11-29 00:13:51 +0800

    Update TitleScreenState.lua
    Line 12 to 55, It allows the user to choose mode(night or day) of the game

--- fifty bird source code/states/TitleScreenState.lua
+++ fifty bird source code/states/TitleScreenState.lua
@@ -1,6 +1,5 @@
 --[[
     TitleScreenState Class
-    
     Author: Colton Ogden
     cogden@cs50.harvard.edu
 
@@ -10,20 +9,47 @@
 
 TitleScreenState = Class{__includes = BaseState}
 
-function TitleScreenState:init()
-    -- nothing
-end
+-- to highlight, whether 'day mode' or 'night mode' of the game
+local highlight = 1
 
 function TitleScreenState:update(dt)
+    -- transition to countdown when enter/return are pressed
     if love.keyboard.wasPressed('enter') or love.keyboard.wasPressed('return') then
+        sounds['select']:play()
         gStateMachine:change('countdown')
     end
+    
+    if love.keyboard.wasPressed('up') or love.keyboard.wasPressed('down') then
+        highlight = highlight == 1 and 2 or 1
+    end
+
+    -- to change background
+    if love.keyboard.wasPressed('up') or love.keyboard.wasPressed('down') then
+        if highlight == 1 then
+            background = true
+            sounds['select']:play()
+        else
+            background = false
+            sounds['select']:play()
+        end
+    end
 end
-
+ 
 function TitleScreenState:render()
+    -- simple UI code
     love.graphics.setFont(flappyFont)
-    love.graphics.printf('Fifty Bird', 0, 64, VIRTUAL_WIDTH, 'center')
-
+    love.graphics.printf('Fifty Bird', 0, 80, VIRTUAL_WIDTH, 'center')
     love.graphics.setFont(mediumFont)
-    love.graphics.printf('Press Enter', 0, 100, VIRTUAL_WIDTH, 'center')
+    love.graphics.printf('Press Enter to Play', 0, 250, VIRTUAL_WIDTH, 'center')
+    love.graphics.setFont(smallFont)
+    love.graphics.setColor(97/255, 107/255, 97/255, 1)
+    -- to display highlight of mode
+    if highlight == 1 then 
+        love.graphics.rectangle('line', 227, 130, 57, 15)
+    else
+        love.graphics.rectangle('line', 227, 148, 57, 15)
+    end
+    love.graphics.setColor(1, 1, 1, 1)
+    love.graphics.printf('Day Mode', 0, 133, VIRTUAL_WIDTH, 'center')
+    love.graphics.printf('Night Mode', 0, 151, VIRTUAL_WIDTH, 'center')
 end
