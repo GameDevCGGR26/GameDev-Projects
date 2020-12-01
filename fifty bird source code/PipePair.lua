480e0964a3be2d318a045c74ab805c13406a4445
Francesca Cruz <70466079+VoidFrancescaCruz@users.noreply.github.com>
2020-12-01 20:01:47 +0800

    Update PipePair.lua

--- fifty bird source code/PipePair.lua
+++ fifty bird source code/PipePair.lua
@@ -26,7 +26,7 @@ function PipePair:init(y)
     -- instantiate two pipes that belong to this pair
     self.pipes = {
         ['upper'] = Pipe('top', self.y),
-        ['lower'] = Pipe('bottom', self.y + PIPE_HEIGHT + math.random(GAP_HEIGHT - 50 , GAP_HEIGHT)) -- to randomize gap height
+        ['lower'] = Pipe('bottom', self.y + PIPE_HEIGHT + math.random(GAP_HEIGHT - 55 , GAP_HEIGHT)) -- to randomize gap height
     }
 
     -- whether this pipe pair is ready to be removed from the scene
