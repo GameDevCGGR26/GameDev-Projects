78cefab1436d63a98f29afb5016080bd4dec7492
Chelsea Garcia <72681344+chelseagarcia0@users.noreply.github.com>
2020-12-19 19:50:17 +0800

    Update Tile.lua
    line 16 to 50, shiny version of tiles

--- match-3/src/Tile.lua
+++ match-3/src/Tile.lua
@@ -13,7 +13,8 @@
 
 Tile = Class{}
 
-function Tile:init(x, y, color, variety)
+function Tile:init(x, y, color, variety, shinyTiles)
+    
     -- board positions
     self.gridX = x
     self.gridY = y
@@ -21,31 +22,32 @@ function Tile:init(x, y, color, variety)
     -- coordinate positions
     self.x = (self.gridX - 1) * 32
     self.y = (self.gridY - 1) * 32
-
+    
+    self.shiny = false
     -- tile appearance/points
     self.color = color
     self.variety = variety
-end
-
-function Tile:update(dt)
-
-end
-
---[[
-    Function to swap this tile with another tile, tweening the two's positions.
-]]
-function Tile:swap(tile)
-
+    self.shinyTiles = shinyTiles
+    
+    -- time for color change or shine tiles
+    self.colorTimer = Timer.every(0.75, function()
+        self.shiny = not self.shiny
+    end)
 end
 
 function Tile:render(x, y)
+    
     -- draw shadow
-    love.graphics.setColor(34, 32, 52, 255)
+    love.graphics.setColor(34/255, 32/255, 52/255, 1)
     love.graphics.draw(gTextures['main'], gFrames['tiles'][self.color][self.variety],
         self.x + x + 2, self.y + y + 2)
 
     -- draw tile itself
-    love.graphics.setColor(255, 255, 255, 255)
+    if self.shinyTiles == true and self.shiny then
+        love.graphics.setColor(1, 1, 1, 40/255)
+    else
+        love.graphics.setColor(1, 1, 1, 1)
+    end
     love.graphics.draw(gTextures['main'], gFrames['tiles'][self.color][self.variety],
         self.x + x, self.y + y)
 end
