00b150fdbd6fc887146a3ecc335038586fe7713d
Chelsea Garcia <72681344+chelseagarcia0@users.noreply.github.com>
2020-12-23 16:42:29 +0800

    Update BeginGameState.lua
    line 80, 82, 88

--- match-3/src/states/BeginGameState.lua
+++ match-3/src/states/BeginGameState.lua
@@ -77,14 +77,14 @@ function BeginGameState:render()
     self.board:render()
 
     -- render Level # label and background rect
-    love.graphics.setColor(95, 205, 228, 200)
+    love.graphics.setColor(95/255, 205/255, 228/255, 200/255)
     love.graphics.rectangle('fill', 0, self.levelLabelY - 8, VIRTUAL_WIDTH, 48)
-    love.graphics.setColor(255, 255, 255, 255)
+    love.graphics.setColor(1, 1, 1, 1)
     love.graphics.setFont(gFonts['large'])
     love.graphics.printf('Level ' .. tostring(self.level),
         0, self.levelLabelY, VIRTUAL_WIDTH, 'center')
 
     -- our transition foreground rectangle
-    love.graphics.setColor(255, 255, 255, self.transitionAlpha)
+    love.graphics.setColor(1, 1, 1, self.transitionAlpha)
     love.graphics.rectangle('fill', 0, 0, VIRTUAL_WIDTH, VIRTUAL_HEIGHT)
 end
