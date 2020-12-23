5fbb1fe04b2090e9cf295c81d536e24c5a444816
Francesca Cruz <70466079+VoidFrancescaCruz@users.noreply.github.com>
2020-12-23 19:57:20 +0800

    Line 55
    update the background image

--- match-3/src/Dependencies.lua
+++ match-3/src/Dependencies.lua
@@ -52,7 +52,7 @@ gSounds = {
 
 gTextures = {
     ['main'] = love.graphics.newImage('graphics/match3.png'),
-    ['background'] = love.graphics.newImage('graphics/background.png')
+    ['background'] = love.graphics.newImage('graphics/background4.png')
 }
 
 gFrames = {
