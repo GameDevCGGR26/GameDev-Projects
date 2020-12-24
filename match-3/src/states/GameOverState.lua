14ece3b9712e04bed297b32d1b6ba2d3db2f659a
Chelsea Garcia <72681344+chelseagarcia0@users.noreply.github.com>
2020-12-25 00:31:05 +0800

    line 24-26

--- match-3/src/states/GameOverState.lua
+++ match-3/src/states/GameOverState.lua
@@ -21,6 +21,9 @@ function GameOverState:enter(params)
 end
 
 function GameOverState:update(dt)
+    if love.keyboard.wasPressed('escape') then
+        love.event.quit()
+    end
     if love.keyboard.wasPressed('enter') or love.keyboard.wasPressed('return') then
         gStateMachine:change('start')
     end
