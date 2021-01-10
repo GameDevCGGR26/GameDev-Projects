12e2382e3d3604d0f517dc2d64e50665e55351be
Loi-cell <74853041+Loi-cell@users.noreply.github.com>
2021-01-10 16:49:36 +0800

    Flag Update

--- mario/src/Dependencies.lua
+++ mario/src/Dependencies.lua
@@ -74,7 +74,9 @@ gTextures = {
     ['backgrounds'] = love.graphics.newImage('graphics/backgrounds.png'),
     ['green-alien'] = love.graphics.newImage('graphics/green_alien.png'),
     ['creatures'] = love.graphics.newImage('graphics/creatures.png'),
-    ['keys-locks'] = love.graphics.newImage('graphics/keys_and_locks.png')
+    ['keys-locks'] = love.graphics.newImage('graphics/keys_and_locks.png'),
+    ['flagposts'] = love.graphics.newImage('graphics/flagposts.png'),
+    ['flags'] = love.graphics.newImage('graphics/flags.png')
 }
 
 gFrames = {
@@ -88,7 +90,9 @@ gFrames = {
     ['backgrounds'] = GenerateQuads(gTextures['backgrounds'], 256, 128),
     ['green-alien'] = GenerateQuads(gTextures['green-alien'], 16, 20),
     ['creatures'] = GenerateQuads(gTextures['creatures'], 16, 16),
-    ['keys-locks'] = GenerateQuads(gTextures['keys-locks'], 16, 16)
+    ['keys-locks'] = GenerateQuads(gTextures['keys-locks'], 16, 16),
+    ['flagposts'] = GenerateQuads(gTextures['flagposts'], 8, 48),
+    ['flags'] = GenerateQuads(gTextures['flags'], 16, 10)
 }
 
 -- these need to be added after gFrames is initialized because they refer to gFrames from within
