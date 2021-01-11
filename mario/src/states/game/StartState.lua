84c6ff90416f130693c0c0c42a333dae87c3b4a3
Chelsea Garcia <72681344+chelseagarcia0@users.noreply.github.com>
2021-01-11 21:31:05 +0800

    Update StartState.lua

--- mario/src/states/game/StartState.lua
+++ mario/src/states/game/StartState.lua
@@ -17,7 +17,7 @@ end
 
 function StartState:update(dt)
     if love.keyboard.wasPressed('enter') or love.keyboard.wasPressed('return') then
-        gStateMachine:change('play')
+        gStateMachine:change('play', {score = 0 , lastLevelWidth = 0})
     end
 end
 
