43fbb695e07497ba8b494d49535682009f12ab31
DanielRanay <72848844+DanielRanay@users.noreply.github.com>
2021-01-19 16:36:31 +0800

    Powerups
    line 69

--- breakout soruce code/main.lua
+++ breakout soruce code/main.lua
@@ -66,6 +66,7 @@ function love.load()
         ['paddles'] = GenerateQuadsPaddles(gTextures['main']),
         ['balls'] = GenerateQuadsBalls(gTextures['main']),
         ['bricks'] = GenerateQuadsBricks(gTextures['main']),
+        ['powerups'] = GenerateQuadsPowerups(gTextures['main']),
         ['hearts'] = GenerateQuads(gTextures['hearts'], 10, 9)
     }
     
