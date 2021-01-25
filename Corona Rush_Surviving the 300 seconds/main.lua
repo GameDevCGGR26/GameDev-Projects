44bfb8840ac9764487fe7f18375788dcafdca435
Francesca Cruz <70466079+VoidFrancescaCruz@users.noreply.github.com>
2021-01-25 14:06:58 +0800

    Update main.lua

--- Corona Rush_Surviving the 300 seconds/main.lua
+++ Corona Rush_Surviving the 300 seconds/main.lua
@@ -9,17 +9,41 @@ love.graphics.setDefaultFilter('nearest', 'nearest')
 
 require 'src/Dependencies'
 
+playing = true
+titleState = true
 
 function love.load()
+
+  push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+    fullscreen = false,
+    resizable = false,
+    vsync = true
+  })
+
   Map = STI("map/level1.lua", {"box2d"}) --loads the map from tiled software
   World = love.physics.newWorld(0, 0)  --creates the world
-  World:setCallbacks(beginContact, endContact) --callbacks for character collision
+ World:setCallbacks(beginContact, endContact) --callbacks for character collision
   Map:box2d_init(World)
-  Map.layers.solid.visible = false
+ Map.layers.solid.visible = false
   MapWidth = Map.width * Map.tilewidth
   MapHeight = Map.height * Map.tileheight
 
-  background = love.graphics.newImage("assets/citybig.png")   --bg insert
+  gStateMachine = StateMachine {
+    ['start'] = function() return StartState() end,
+    ['play'] = function() return PlayState() end,
+    ['game-over'] = function() return GameOverState() end,
+    ['controls'] = function() return ControlState() end
+  }
+  gStateMachine:change('start')
+
+  gSounds['level1']:setLooping(true)
+  gSounds['level1']:setVolume(0.5)
+  gSounds['level1']:play()
+
+
+  love.keyboard.keysPressed = {}
+
+  --background = love.graphics.newImage("assets/citybig.png")   --bg insert
   GUI:load()
   Player:load()
   Coin:new(400,100)
@@ -37,53 +61,43 @@ function love.load()
 
 end
 
-function  love.update(dt)
-    World:update(dt)
-    Player:update(dt)
-    Coin.updateAll(dt)
-    Mask.updateAll(dt)
-    Faceshield.updateAll(dt)
-    Ppe.updateAll(dt)
-    Alcohol.updateAll(dt)
-    GUI:update(dt)
-
-    Camera:setPosition(Player.x, 0)
-  --backgroundScroll = (backgroundScroll + BACKGROUND_SCROLL_SPEED * dt)
+function love.resize(w, h)
+	push:resize(w, h)
+	print(w, h)
 end
 
+
+
+
 function love.draw()    --map insert and bg
-  love.graphics.draw(gTextures.background, 0, 0, 0, 4, 3.5)
-  Map:draw(-Camera.x, -Camera.y, Camera.scale, Camera.scale)
-
-  Camera:apply()
-    Player:draw()
-    Coin.drawAll()
-    Mask.drawAll()
-    Faceshield.drawAll()
-    Ppe.drawAll()
-    Alcohol.drawAll()
-  Camera:clear()
-
-    GUI:draw()
+--push:start()
+  gStateMachine:render()
+ --push:finish()
 end
 
 function love.keypressed (key)
+  love.keyboard.keysPressed[key] = true
   Player:jump(key)
   Player:fire(key)
-  if key == 'escape' then
-    love.event.quit()
-  end
+
+
+end
+
+function love.keyboard.wasPressed(key)
+	return love.keyboard.keysPressed[key]
+end
+
+function  love.update(dt)
+  	gStateMachine:update(dt)
+    love.keyboard.keysPressed = {}
+  --backgroundScroll = (backgroundScroll + BACKGROUND_SCROLL_SPEED * dt)
 end
 
 function beginContact (a, b, collision)
-  if Coin:beginContact(a,b, collision) then return end
-  if Mask.beginContact(a,b, collision) then return end
-  if Faceshield.beginContact(a,b, collision) then return end
-  if Ppe.beginContact(a,b, collision) then return end
-  if Alcohol.beginContact(a,b, collision) then return end
-  Player:beginContact (a, b, collision)
+  gStateMachine:beginContact(a, b, collision)
+
 end
 
 function endContact (a, b, collision)
-    Player:endContact (a, b, collision)
+    	gStateMachine:endContact(a, b, collision)
 end
