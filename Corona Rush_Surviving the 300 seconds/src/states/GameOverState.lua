52a21fd96908046914ffa8cfc7d9843fd6ce71a9
Loi-cell <74853041+Loi-cell@users.noreply.github.com>
2021-01-25 18:03:53 +0800

    Update GameOverState.lua

--- Corona Rush_Surviving the 300 seconds/src/states/GameOverState.lua
+++ Corona Rush_Surviving the 300 seconds/src/states/GameOverState.lua
@@ -15,17 +15,19 @@ function GameOverState:update(dt)
     if love.keyboard.wasPressed('enter') or love.keyboard.wasPressed('return') then
         gStateMachine:change('start')
     end
+    
+    if love.keyboard.wasPressed('space') then
+        gStateMachine:change('play')
+    end
 end
 
 function GameOverState:render()
-   
-        love.graphics.setFont(gFonts['medium'])
-        love.graphics.setColor(99, 155, 255, 255)
-        love.graphics.printf('GAME OVER', 1, VIRTUAL_HEIGHT / 2 - 40 + 1, VIRTUAL_WIDTH, 'center')
-        love.graphics.setFont(gFonts['small'])
-        love.graphics.printf('You have died', 1, VIRTUAL_HEIGHT / 2 - 40 + 20, VIRTUAL_WIDTH, 'center')
-        love.graphics.printf('Press Enter to Go to MAIN MENU', 1, VIRTUAL_HEIGHT / 2 + 15, VIRTUAL_WIDTH, 'center')
-   
-
+    love.graphics.setFont(gFonts.medium)
+    love.graphics.setColor(99, 155, 255, 255)
+    love.graphics.printf('GAME OVER', 150, 100, 1000, 'center')
+    love.graphics.setFont(gFonts.small)
+    love.graphics.printf('You have died', 530, 400, 1000)
+    love.graphics.printf('Press ENTER to Go to MAIN MENU', 150, 450, 1000, 'center')
+    love.graphics.printf('Press SPACE to RESTART', 150, 500, 1000, 'center')
 end
 
