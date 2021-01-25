1f1c72a20878fb4d43b9ad973d6168c22b550200
Chelsea Garcia <72681344+chelseagarcia0@users.noreply.github.com>
2021-01-25 16:37:37 +0800

    Update StartState.lua

--- Corona Rush_Surviving the 300 seconds/src/states/StartState.lua
+++ Corona Rush_Surviving the 300 seconds/src/states/StartState.lua
@@ -50,42 +50,42 @@ function StartState:update(dt)
 
 function StartState:render()
 
-    love.graphics.draw(gTextures.toxic2)
+    love.graphics.draw(gTextures.toxic2, 0, 0, 0, 5, 5)
 
     love.graphics.setFont(gFonts.large)
     love.graphics.setColor(0, 0, 0, 255)
-    love.graphics.printf('Corona Rush', 1, VIRTUAL_HEIGHT / 2 - 40 + 1, VIRTUAL_WIDTH, 'center')
+    love.graphics.printf('Corona Rush', 3, WINDOW_HEIGHT / 4 + 5, WINDOW_WIDTH, 'center')
     love.graphics.setColor(255, 255, 255, 255)
-    love.graphics.printf('Corona Rush', 0, VIRTUAL_HEIGHT / 2 - 40, VIRTUAL_WIDTH, 'center')
+    love.graphics.printf('Corona Rush', 0, WINDOW_HEIGHT / 4, WINDOW_WIDTH, 'center')
 
     love.graphics.setFont(gFonts.small)
     love.graphics.setColor(0, 0, 0, 255)
-    love.graphics.printf('Controls', 50, VIRTUAL_HEIGHT / 2 + 38, VIRTUAL_WIDTH)
+    love.graphics.printf('Controls', 300, WINDOW_HEIGHT / 2 + 213, WINDOW_WIDTH)
     love.graphics.setColor(255, 255, 255, 255)
-    love.graphics.printf('Controls', 49, VIRTUAL_HEIGHT / 2 + 37, VIRTUAL_WIDTH)
+    love.graphics.printf('Controls', 297, WINDOW_HEIGHT / 2 + 210, WINDOW_WIDTH)
 
     love.graphics.setFont(gFonts.medium)
     love.graphics.setColor(0, 0, 0, 255)
-    love.graphics.printf('Start', 1, VIRTUAL_HEIGHT / 2 + 34, VIRTUAL_WIDTH, 'center')
+    love.graphics.printf('Start', 3, WINDOW_HEIGHT / 2 + 203, WINDOW_WIDTH, 'center')
     love.graphics.setColor(255, 255, 255, 255)
-    love.graphics.printf('Start', 0, VIRTUAL_HEIGHT / 2 + 33, VIRTUAL_WIDTH, 'center')
+    love.graphics.printf('Start', 0, WINDOW_HEIGHT / 2 + 200, WINDOW_WIDTH, 'center')
 
     love.graphics.setFont(gFonts.small)
     love.graphics.setColor(0, 0, 0, 255)
-    love.graphics.printf('Exit', 175, VIRTUAL_HEIGHT / 2 + 38, VIRTUAL_WIDTH)
+    love.graphics.printf('Exit', WINDOW_WIDTH - 450, WINDOW_HEIGHT / 2 + 213, WINDOW_WIDTH)
     love.graphics.setColor(255, 255, 255, 255)
-    love.graphics.printf('Exit', 174, VIRTUAL_HEIGHT / 2 + 37, VIRTUAL_WIDTH)
+    love.graphics.printf('Exit', WINDOW_WIDTH - 447, WINDOW_HEIGHT / 2 + 210, WINDOW_WIDTH)
 
     if highlighted == 1 then
         love.graphics.setColor(255/255, 255/255, 0, 255/255)
-        love.graphics.printf('Controls', 49, VIRTUAL_HEIGHT / 2 + 37, VIRTUAL_WIDTH)
+        love.graphics.printf('Controls', 297, WINDOW_HEIGHT / 2 + 210, WINDOW_WIDTH)
     elseif highlighted == 2 then
         love.graphics.setFont(gFonts.medium)
         love.graphics.setColor(255/255, 255/255, 0, 255/255)
-        love.graphics.printf('Start', 0, VIRTUAL_HEIGHT / 2 + 33, VIRTUAL_WIDTH, 'center')
+        love.graphics.printf('Start', 0, WINDOW_HEIGHT / 2 + 200, WINDOW_WIDTH, 'center')
     elseif highlighted == 3 then
         love.graphics.setColor(255/255, 255/255, 0, 255/255)
-        love.graphics.printf('Exit', 174, VIRTUAL_HEIGHT / 2 + 37, VIRTUAL_WIDTH)
+        love.graphics.printf('Exit', WINDOW_WIDTH - 447, WINDOW_HEIGHT / 2 + 210, WINDOW_WIDTH)
     else
     end
 
