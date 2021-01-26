94aebb5c84fa4c658daca71b02c5539e2f98192b
Chelsea Garcia <72681344+chelseagarcia0@users.noreply.github.com>
2021-01-26 23:40:13 +0800

    Update ppe.lua

--- Corona Rush_Surviving the 300 seconds/src/powerups/ppe.lua
+++ Corona Rush_Surviving the 300 seconds/src/powerups/ppe.lua
@@ -3,23 +3,22 @@ Ppe.__index = Ppe
 ActivePpes = {}
 
 function Ppe:new (x,y)
-local  instance = setmetatable ({}, Ppe)
-instance.x = x
-instance.y = y
---instance.img = love.graphics.newImage("assets/mask.png")
-instance.width = 32
-instance.height = 32
-instance.scaleX = 1
-instance.randomTimeOffset = math.random(0, 100)
-instance.toBeRemoved = false
+  local  instance = setmetatable ({}, Ppe)
+  instance.x = x
+  instance.y = y
+  instance.width = 32
+  instance.height = 32
+  instance.scaleX = 1
+  instance.randomTimeOffset = math.random(0, 100)
+  instance.toBeRemoved = false
 
-instance:loadAssets()
-instance.physics = {}
-instance.physics.body = love.physics.newBody(World, instance.x, instance.y, "static")
-instance.physics.shape = love.physics.newRectangleShape(instance.width, instance.height)
-instance.physics.fixture = love.physics.newFixture(instance.physics.body, instance.physics.shape)
-instance.physics.fixture:setSensor(true)
-table.insert(ActivePpes, instance)
+  instance:loadAssets()
+  instance.physics = {}
+  instance.physics.body = love.physics.newBody(World, instance.x, instance.y, "static")
+  instance.physics.shape = love.physics.newRectangleShape(instance.width, instance.height)
+  instance.physics.fixture = love.physics.newFixture(instance.physics.body, instance.physics.shape)
+  instance.physics.fixture:setSensor(true)
+  table.insert(ActivePpes, instance)
 end
 
 function Ppe:loadAssets()
@@ -33,7 +32,6 @@ end
 function Ppe:remove ()
   for i,instance in ipairs(ActivePpes) do
     if instance == self then
-      --Player:incrementPowerups()
       self.physics.body:destroy()
       table.remove(ActivePpes, i)
     end
@@ -41,9 +39,15 @@ function Ppe:remove ()
 end
 
 function Ppe:update (dt)
---self:spin(dt)
-self.currentAnimation:update(dt)
-self:checkRemove()
+  self.currentAnimation:update(dt)
+  self:checkRemove()
+  self:checkNumber()
+end
+
+function Ppe:checkNumber()
+  if #ActivePpes > 1 then
+     self:remove()
+  end
 end
 
 function Ppe:checkRemove ()
@@ -52,12 +56,7 @@ function Ppe:checkRemove ()
   end
 end
 
---function Mask:spin (dt)
---self.scaleX = math.sin(love.timer.getTime() * 2 + self.randomTimeOffset)
---end
-
 function Ppe:draw ()
-  -- love.graphics.draw(self.img, self.x, self.y, 0, self.scaleX, 1, self.width/2, self.height/2)
   love.graphics.draw(
   gTextures.ppe, gFrames.ppe[self.currentAnimation:getCurrentFrame()],
       self.x, self.y, 0, scaleX, 1, self.width/2, self.height/2
