2dca60914e88131ec1b2d9a7169056771e368525
Chelsea Garcia <72681344+chelseagarcia0@users.noreply.github.com>
2021-01-26 23:37:11 +0800

    Update PlayState.lua

--- Corona Rush_Surviving the 300 seconds/src/states/PlayState.lua
+++ Corona Rush_Surviving the 300 seconds/src/states/PlayState.lua
@@ -6,15 +6,34 @@ function PlayState:init()
     -- self.playing = true
     titleState = false
     playing = true
-
+    
+ 
+ 
     -- Slime.loadAssets()
 --    Map:load()
     -- Player:load()
+    GUI:load()
+    Player:load()
+    Coin:new(400,100)
+    Coin:new(2895,170)
+    Coin:new(5000,230)
+  
+    Mask:new(3300,160)
+    Mask:new(830,130)
+  
+    Faceshield:new(4450,150)
+  
+    Ppe:new(1380,150)
+  
+    Alcohol:new(2260,170)
     GUI:load()
 
     TIMERS = 300
     Timer.every(1, function()
             TIMERS = TIMERS - 1
+          end)
+          Timer.every(intervals[i], function()
+            counters[i] = counters[i] + 1
         end)
 end
 
@@ -34,6 +53,11 @@ function PlayState:update(dt)
     if TIMERS <= 0 then
       -- clear timers from prior PlayStates
       Timer.clear()
+      Coin.updateAll(dt)
+      Mask.updateAll(dt)
+      Faceshield.updateAll(dt)
+      Ppe.updateAll(dt)
+      Alcohol.updateAll(dt)
 
       gSounds['death']:play()
 
@@ -41,24 +65,35 @@ function PlayState:update(dt)
          timer = TIMERS
       })
    end
-  --  Map:update(dt)
-
+    --  Map:update(dt) 
+    local halfScreen =  WINDOW_WIDTH / 2
+    
+    if Player.x < (MapWidth - halfScreen) then
+        boundX = math.max(0, Player.x - halfScreen)
+    else
+        boundX = math.min(Player.x - halfScreen, MapWidth - WINDOW_WIDTH)
+    end
+    
     Camera:setPosition(Player.x, 0)
+    
+    BACKGROUND_SCROLL = (boundX/3) % 524 * 4
 
-      if love.keyboard.wasPressed('escape') then
-          if playing then
-              playing = false
-          else
-              playing = true
-          end
-      end
+    if love.keyboard.wasPressed('escape') then
+        if playing then
+            playing = false
+        else
+            playing = true
+        end
+    end
+
+      
   end
 
 
 
 
 function PlayState:render()
-  love.graphics.draw(gTextures.background, 0, 0, 0, 4, 3.5)
+  love.graphics.draw(gTextures.background, -BACKGROUND_SCROLL, 0, 0, 4, 3.5)
 	Map:draw(-Camera.x, -Camera.y, Camera.scale, Camera.scale)
 
 	Camera:apply()
@@ -78,11 +113,11 @@ end
 
 
 function PlayState:beginContact(a, b, collision)
-  if Coin:beginContact(a,b, collision) then return end
-  if Mask.beginContact(a,b, collision) then return end
-  if Faceshield.beginContact(a,b, collision) then return end
-  if Ppe.beginContact(a,b, collision) then return end
-  if Alcohol.beginContact(a,b, collision) then return end
+  Coin:beginContact(a,b, collision)
+  Mask.beginContact(a,b, collision)
+  Faceshield.beginContact(a,b, collision)
+  Ppe.beginContact(a,b, collision)
+  Alcohol.beginContact(a,b, collision)
   Player:beginContact (a, b, collision)
 end
 
