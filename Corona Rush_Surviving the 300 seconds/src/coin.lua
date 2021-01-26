764a617bbac9f742c3d21e1897d57e749e662916
Chelsea Garcia <72681344+chelseagarcia0@users.noreply.github.com>
2021-01-26 23:36:21 +0800

    Update coin.lua

--- Corona Rush_Surviving the 300 seconds/src/coin.lua
+++ Corona Rush_Surviving the 300 seconds/src/coin.lua
@@ -3,23 +3,23 @@ Coin.__index = Coin
 ActiveCoins = {}
 
 function Coin:new (x,y)
-local  instance = setmetatable ({}, Coin)
-instance.x = x
-instance.y = y
--- instance.img = love.graphics.newImage("assets/coinsingle.png")
-instance.width = 16
-instance.height = 16
-instance.scaleX = 1
-instance.randomTimeOffset = math.random(0, 100)
-instance.toBeRemoved = false
+  local  instance = setmetatable ({}, Coin)
+  instance.x = x
+  instance.y = y
+  -- instance.img = love.graphics.newImage("assets/coinsingle.png")
+  instance.width = 16
+  instance.height = 16
+  instance.scaleX = 1
+  instance.randomTimeOffset = math.random(0, 100)
+  instance.toBeRemoved = false
 
-instance:loadAssets()
-instance.physics = {}
-instance.physics.body = love.physics.newBody(World, instance.x, instance.y, "static")
-instance.physics.shape = love.physics.newRectangleShape(instance.width, instance.height)
-instance.physics.fixture = love.physics.newFixture(instance.physics.body, instance.physics.shape)
-instance.physics.fixture:setSensor(true)
-table.insert(ActiveCoins, instance)
+  instance:loadAssets()
+  instance.physics = {}
+  instance.physics.body = love.physics.newBody(World, instance.x, instance.y, "static")
+  instance.physics.shape = love.physics.newRectangleShape(instance.width, instance.height)
+  instance.physics.fixture = love.physics.newFixture(instance.physics.body, instance.physics.shape)
+  instance.physics.fixture:setSensor(true)
+  table.insert(ActiveCoins, instance)
 end
 
 function Coin:loadAssets()
@@ -30,7 +30,7 @@ function Coin:loadAssets()
   self.currentAnimation = self.animation
 end
 
-function Coin:remove ()
+function Coin:remove()
   for i,instance in ipairs(ActiveCoins) do
     if instance == self then
       Player:incrementCoins()
@@ -40,43 +40,41 @@ function Coin:remove ()
   end
 end
 
-function Coin:update (dt)
--- self:spin(dt)
-self.currentAnimation:update(dt)
-self:checkRemove()
+function Coin:update(dt)
+  self.currentAnimation:update(dt)
+  self:checkRemove()
 end
 
-function Coin:checkRemove ()
+function Coin:checkRemove()
   if self.toBeRemoved then
    self:remove()
   end
 end
 
-function Coin:spin (dt)
+function Coin:spin(dt)
   self.scaleX = math.sin(love.timer.getTime() * 2 + self.randomTimeOffset)
 end
 
-function Coin:draw ()
-  -- love.graphics.draw(self.img, self.x, self.y, 0, self.scaleX, 1, self.width/2, self.height/2)
+function Coin:draw()
   love.graphics.draw(
       gTextures.coins, gFrames.coins[self.currentAnimation:getCurrentFrame()],
       self.x, self.y, 0, scaleX, 1, self.width/2, self.height/2
    )
 end
 
-function Coin.updateAll (dt)
+function Coin.updateAll(dt)
   for i,instance in ipairs(ActiveCoins) do
       instance:update(dt)
   end
 end
 
-function Coin.drawAll ()
+function Coin.drawAll()
   for i,instance in ipairs(ActiveCoins) do
       instance:draw()
   end
 end
 
-function Coin:beginContact (a, b, collision)
+function Coin:beginContact(a, b, collision)
   for i,instance in ipairs(ActiveCoins) do
     if a == instance.physics.fixture or b == instance.physics.fixture then
       if a == Player.character.fixture or b == Player.character.fixture then
