61ba96d854458a98cf82c6f3f859ed78be3b4057
Francesca Cruz <70466079+VoidFrancescaCruz@users.noreply.github.com>
2021-01-27 22:43:54 +0800

    Add files via upload

---
+++ Corona Rush_Surviving the 300 seconds/src/states/NextLevelState.lua
@@ -0,0 +1,32 @@
+
+NextLevelState = Class{__includes = BaseState}
+
+
+
+function NextLevelState:init()
+
+end
+
+function NextLevelState:enter()
+
+end
+
+function NextLevelState:update(dt)
+    if love.keyboard.wasPressed('enter') then
+        gStateMachine:change('play')
+    end
+
+    if love.keyboard.wasPressed('escape') then
+        gStateMachine:change('start')
+    end
+end
+
+function NextLevelState:render()
+    love.graphics.setFont(gFonts.medium)
+    love.graphics.setColor(99, 155, 255, 255)
+    love.graphics.printf('CONGRATULATIONS', 150, 100, 1000, 'center')
+    love.graphics.setFont(gFonts.small)
+    love.graphics.printf('You can now proceed to the next level', 230, 400, 1000)
+    love.graphics.printf('Press ENTER to proceed to Level 2!', 150, 450, 1000, 'center')
+    love.graphics.printf('Press ESCAPE to Go back to the MAIN MENU', 150, 500, 1000, 'center')
+end
