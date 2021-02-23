0a1ae0c2db0b4099bc5e3c84b8bb89213b14f301
Francesca Cruz <70466079+VoidFrancescaCruz@users.noreply.github.com>
2021-02-23 11:30:17 +0800

    Add files via upload

---
+++ Corona Rush_Surviving the 300 seconds/src/cutscenes/cutscene1.lua
@@ -0,0 +1,143 @@
+dialogue_Cutscene1 = {
+    "News Anchor\n\n\nBREAKING NEWS!!",  
+    
+    "News Anchor\n\n\nThe miasma in the air is killing people in alarming rate. This is caused by an unknown virus.",
+    
+    "News Anchor\n\n\nWe have no idea what this virus, so PLEASE STAY AT HOME. I REPEAT EVERYONE PLEASE STAY\nAT--------.", 
+        
+    "\n\n\n4 months later....", 
+    
+    "Cesca\n\n(The stark smell of bleach was overwhelming.  I scrunched my eyes at the dark and ominous\nnothingness that was sipping through my closed eyelids.)", 
+    
+    "Cesca\n\n(I slowly started blinking, desperately trying to open my eyes and see what is around\nme.)",
+    
+    "Cesca\n\nUgh..... my head hurts..... Ugh.....Where am I?", 
+    
+    "Cesca\n\n(As I opened my eyes, I took a quick look around my surroundings. And I realized, I was\nat a hospital. There is hospital curtains in my peripheral)", 
+    
+    "Cesca\n\nWhat happened?.....", 
+    
+    "Cesca\n\nAghhh.....I can't remember..... What is happening?", 
+    
+    "Cesca\n\n(It was dark. But why was it so dark? If this was a decent hospital, this should have\nsome lighting, but I only saw darkness.)",  
+    
+    "\n\n\nAfter some time thinking about what happened, she had a realization.", 
+    
+    "Cesca\n\n(Wait, where are the others?)", 
+    
+    "Cesca\n\n(I slide the curtains to the side of the hospital ward boundary)", 
+    
+    "\n\n\nThere she saw a person in that hospital bed that looked similar to a friend of hers in an\noxygen mask, similar to what she had before.", 
+    
+    "Cesca\n\n(In response and pure worrisome, I got out of my bed and put my hands in her\nshoulders and  tried to forcibly wake her up.)", 
+    
+    "Cesca\n\nChelsea! Chelsea! Wake up! Chelsea!",  
+    
+    "????\n\nUgh..... Why is it so noisy?",  
+    
+    "Cesca\n\nHuh! Che! Are you ok?",  
+    
+    "Chelsea\n\nWait, uhhh.....Cesca? Why? What is happening?", 
+    
+    "Cesca\n\nI also don't know but most importantly, are you ok? Are you hurt?", 
+    
+    "Chelsea\n\nI'm fine. I'm fine.", 
+    
+    "Cesca\n\nPhew. That's good. But I know this a sudden question and you just woke up but do you\nremember anything, Chelsea? I just can't remember anything.", 
+    
+    "Chelsea\n\nuhh..... Wait, what? Why I can't remember anything? Huh, what? I'm sorry, Cesca I can't\nremember anything as well.", 
+    
+    "Cesca\n\nHmmm....", 
+    
+    "Cesca\n\n(I'm just glad that she's okay)",
+    
+    "",
+    
+    "\n\n\n. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .",
+    
+    "Chelsea\n\nCesca, do you know where everybody are?", 
+    
+    "Cesca\n\nNo, I just woke a minute ago and then I found you here.", 
+        
+    "Chelsea\n\nHave you checked the other rooms yet?",
+        
+    "\n\n\nWithout a moment's notice both of them checked the facilities of the dimly lit hospital\nbuilding.",
+    
+    "\n\n\n. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .",
+    
+    "",
+    
+    "\n\n\nSearching, and searching, and searching.", 
+    
+    "\n\n\nFor hours and hours, yet they only saw two.", 
+    
+    "Chelsea\n\nAre you both okay?",
+    
+    "????\n\nYes, we woke up a while ago and decided to stick together so to not get lost.", 
+    
+    "????\n\nWe tried finding some staff to help us but we couldn't find anyone.", 
+    
+    "Cesca\n\nHmm... first and foremost? Who are the both you?", 
+    
+    "Both of them\n\nHHUUUHHH?", 
+    
+    "????\n\nDon't you remember us? We're your classmates. I'm Lois.", 
+    
+    "????\n\nAnd I'm Daniel. We're always at the back not participating in the class. Maybe that's\nwhy you don't remember our names.", 
+    
+    "Chelsea\n\nOHHH! You're the tall guy and the athlete, now I remember.", 
+    
+    "Cesca\n\n(Why are they the only one we saw? Where is everybody?)", 
+    
+    "\n\n\nWhile wondering and thinking, Cesca took a glimpse at the window. Her eyes widen the\nmoment she saw it.", 
+    
+    "Cesca\n\nuhh.....Guys, look at this first.", 
+    
+    "Chelsea\n\nWait, why?",
+    
+    "Chelsea\n\nWhat happened outside?", 
+    
+    "\n\n\nThey all saw that frightening scene. And everyone was full of fear.", 
+    
+    "\n\n\nThe frightened voice of their classmates were apparent in their silence.",
+    
+    "\n\n\n. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .",
+    
+    "",
+    
+    "Cesca\n\nSo, here's my idea.",  
+    
+    "Cesca\n\nOne of us should go outside and ask for help.", 
+    
+    "Everyone\n\nWHAT?!", 
+    
+    "Lois\n\nHave you seen what happened outside? And you want us to go outside? Are you crazy?", 
+    
+    "Chelsea\n\nYeah, Cesca why do this? We're perfectly safe inside this abandoned hospital.Plus,\nthere might be food and clothes lying around the place.", 
+    
+    "Chelsea\n\nSo, why risk it?", 
+    
+    "Cesca\n\nYou see, we can't survive on our own. Maybe we can withstand for a few days but\nif our rations get exhausted, we'll all die without food.",
+    
+    "Cesca\n\nSo, I suggest we go out now before it's too late.", 
+    
+    "Everyone\n\n. . . . . . . . . . . . . . . . . . . . . . .",
+    
+    "Cesca\n\n(That's to be expected. No one wants to volunteer. I mean we were just having a normal\nday yesterday and now we would risk our lives for some classmates. I don't blame\nthem for not volunteering. I guess I need to-)", 
+    
+    "Daniel\n\nI volunteer myself as the one who goes outside.", 
+    
+    "Cesca\n\n(What? Daniel volunteered?)",
+    
+    "Cesca\n\nDaniel, why do you want to go outside?", 
+    
+    "Daniel\n\nWell, because I'm the most equipped person in here who can do that job. Also, I'm\nan athlete so I'm faster than all of you.", 
+    
+    "Cesca\n\nI agree, than you're an athlete but I think I should go.", 
+    
+    "Chelsea\n\nWhy don't we vote? Whoever, thinks that he/she should go outside, would be\nour messenger?", 
+    
+    "Lois\n\nHmph....... just remember I won't be going outside. Deal with that yourselves.", 
+    
+    "Cesca\n\nSo, who would go outside?"
+    }
