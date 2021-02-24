d9f91ecd812b795065168a81dafe9ce8a712a5d6
Chelsea Garcia <72681344+chelseagarcia0@users.noreply.github.com>
2021-02-25 00:14:14 +0800

    Update cutscene2.lua

--- Corona Rush_Surviving the 300 seconds/src/cutscenes/cutscene2.lua
+++ Corona Rush_Surviving the 300 seconds/src/cutscenes/cutscene2.lua
@@ -1,12 +1,12 @@
 dialogue_Cutscene2 = {
 
-    "Player\n\n(Damn, the amount of people here is absurd. Why is this treatment center\nso crowded? I need to get out of here ASAP if I don't want to get sick and\npotentially fail my mission.) ",
+    "\n\nPlayer:\n\t\t(Damn, the amount of people here is absurd. Why is this treatment center so crowded? \n\t\tI need to get out of here ASAP if I don't want to get sick and potentially fail my mission.) ",
     
-    "\n\n\nYou managed to get a vaccine and enough vaccine for your classmates in a\ntimely manner, also without getting in contact with many people. ", 
+    "\n\n\nYou managed to get a vaccine and enough vaccine for your classmates in a timely manner, also without getting in contact with many people. ", 
     
-    "Player\n\nPhew! ",  
+    "\n\nPlayer:\n\t\tPhew! ",  
         
-    "Player\n\n(That was hard. I had to maneuver myself through the crowd. Finally,\nits done. I need to go back to the hospital before they get to much exposure to the virus.) ", 
+    "\n\nPlayer:\n\t\t(That was hard. I had to maneuver myself through the crowd. Finally, its done. I need \n\t\tto go back to the hospital before they get to much exposure to the virus.) ", 
         
     "\n\n\n. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .",
     
@@ -14,21 +14,21 @@ dialogue_Cutscene2 = {
     
     "\n\n\nThe virus mutated.", 
     
-    "\n\n\nDue to the sheer amount of people in that treatment center, the virus\nhad enough time to grow and develop during that time span. ", 
+    "\n\n\nDue to the sheer amount of people in that treatment center, the virus had enough time to grow and develop during that time span. ", 
     
     "\n\n\nPeople weren't cautious enough to protect themselves from the virus. ", 
     
     "\n\n\nSo, the virus grew stronger and more dangerous.", 
     
-    "\n\n\nInfecting every single being it touches and spreading virus molecules\nin the air. It can't be escaped. ", 
+    "\n\n\nInfecting every single being it touches and spreading virus molecules in the air. It can't be escaped. ", 
     
     "\n\n\n. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .",
     
-    "Player\n\nWhat is that soud like someone's chasing me? ",
+    "\n\nPlayer:\n\t\tWhat is that sound like someone's chasing me? ",
     
-    "\n\nThe moment you turned around, there you saw a large amalgamations\nof tranformations. ", 
+    "\n\n\nThe moment you turned around, there you saw a large amalgamations of tranformations. ", 
     
-    "Player\n\nWhat is that?! Is that the virus?! I need to run now before its too late. ", 
+    "\n\nPlayer:\n\t\tWhat is that?! Is that the virus?! I need to run now before its too late. ", 
     
     "\n\n\n. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . ."
     }
