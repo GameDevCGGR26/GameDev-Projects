fff73ca416bc62b1b84e2e2ac32ec2be76382353
Francesca Cruz <70466079+VoidFrancescaCruz@users.noreply.github.com>
2021-02-24 16:13:07 +0800

    Update Cutscene1State.lua

--- Corona Rush_Surviving the 300 seconds/src/states/Cutscene1State.lua
+++ Corona Rush_Surviving the 300 seconds/src/states/Cutscene1State.lua
@@ -64,16 +64,17 @@ function Cutscene1State:update(dt)
         if love.keyboard.wasPressed('space') then
         --controls user input to go to forward
             next_text = next_text + 1
-            x = x + 1
+           x = x + 1
 
 
         elseif love.keyboard.wasPressed('x') then
         --controls user input to go to back
             next_text = next_text - 1
-            x = x - 1
-        elseif love.keyboard.wasPressed('lshift') then
+           x = x - 1
+
+        elseif love.keyboard.wasPressed('s') then
             next_text = 71
-            x = 71
+      x = 71
               gSounds['news']:stop()
         end
 
@@ -175,20 +176,31 @@ function Cutscene1State:render()
         love.graphics.draw(gTextures.dialogboxcs, 0, 515, 0, 1, 1)
     end
 
+
     if dialogue_Cutscene1[next_text] ~= nil then
         if next_text == 1 or next_text == 2 or next_text == 3 then
             love.graphics.setFont(gFonts.xxsmall)
             love.graphics.setColor(1, 1, 1, 1)
             love.graphics.print(dialogue_Cutscene1[next_text], 150, 500)
-            love.graphics.print(next_text, 0, 0)
+          --  love.graphics.print(next_text, 0, 0)
+
         elseif next_text == x then
             love.graphics.setFont(gFonts.xxsmall)
             love.graphics.setColor(1, 1, 1, 1)
             love.graphics.print(dialogue_Cutscene1[next_text], 150, 475)
-            love.graphics.print(next_text, 0, 0)
+            --love.graphics.print(next_text, 0, 0)
         end
 
     end
+    love.graphics.printf("Press s to skip", 20, 670,
+        WINDOW_WIDTH, 'left')
+
+        love.graphics.printf("Press x for previous", 10, 670,
+            WINDOW_WIDTH, 'center')
+
+    love.graphics.printf("Press Spacebar to continue", -20, 670,
+        WINDOW_WIDTH, 'right')
+
 
 end
 
