95b76dfeea3952e7c85b201e7414a42ecd8725eb
Loi-cell <74853041+Loi-cell@users.noreply.github.com>
2021-02-24 14:59:26 +0800

    Update Cutscene2State.lua

--- Corona Rush_Surviving the 300 seconds/src/states/Cutscene2State.lua
+++ Corona Rush_Surviving the 300 seconds/src/states/Cutscene2State.lua
@@ -6,17 +6,37 @@ require 'src/cutscenes/cutscene2'
 function Cutscene2State:init()
     Map:clean()
     Map:init() -- load first
-
+    self:loadAssets()
+    dialog_finished3 = false
+    gSounds['level1']:stop()
 end
 
 function Cutscene2State:enter()
     next_text = 1
     x = 1
     gSounds['level2']:stop()
+    
+    gSounds['bgm2']:setLooping(true)
+    gSounds['bgm2']:setVolume(0.25)
+    gSounds['bgm2']:play()
+  
+end
+
+function Cutscene2State:loadAssets()
+    self.animation = {state}
+
+    self.animation = Animation {
+        frames = {1,2,3,4,5},
+        interval = 1.5,
+    }
+
+
+    self.currentAnimation = self.animation
 end
 
 function Cutscene2State:update(dt)
 
+    self.currentAnimation:update(dt)
 
     if next_text ~= nil then
         --if variable next_text exists
@@ -40,12 +60,40 @@ function Cutscene2State:update(dt)
 
     end
 
+    if self.currentAnimation.currentFrame == 5 then
+        dialog_finished3 = true
+    end
+
+
+    
+
 end
 
 function Cutscene2State:render()
+    love.graphics.draw(gTextures.dialogboxcs, 0, 515, 0, 1, 1)
+    
+    if next_text == 1 or next_text == 3 or next_text == 4 then
+        love.graphics.draw(gTextures.crowd, 0, 0, 0, 5, 5)
+        love.graphics.draw(gTextures.dialogboxcs, 0, 515, 0, 1, 1)
+    end
+
+    if next_text == 13 then
+        love.graphics.draw(gTextures.road, 0, 0, 0, 5, 5)
+        love.graphics.draw(gTextures.dialogboxcs, 0, 515, 0, 1, 1)
+    end
+
+    if next_text == 14 or next_text == 15 then
+        if dialog_finished3 == false then
+          if self.currentAnimation.currentFrame < 5 then
+            love.graphics.draw(gTextures.bigboss, gFrames.bigboss[self.currentAnimation:getCurrentFrame()], 0, 0, 0, 5, 5)
+          end
+        else
+          love.graphics.draw(gTextures.bigboss5, 0, 0, 0, 5, 5)
+        end
+    end
+
     if dialogue_Cutscene2[next_text] ~= nil then
         if next_text == x then
-            love.graphics.draw(gTextures.dialogboxcs, 0, 515, 0, 1, 1)
             love.graphics.setFont(gFonts.xxsmall)
             love.graphics.setColor(1, 1, 1, 1)
             love.graphics.print(dialogue_Cutscene2[next_text], 150, 475)
@@ -53,3 +101,5 @@ function Cutscene2State:render()
         end
     end
 end
+
+
