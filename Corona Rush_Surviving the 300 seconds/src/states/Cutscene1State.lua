579be7077bc25ec14e3deae244ed02e50cf4fe03
Chelsea Garcia <72681344+chelseagarcia0@users.noreply.github.com>
2021-02-25 00:10:22 +0800

    Update Cutscene1State.lua

--- Corona Rush_Surviving the 300 seconds/src/states/Cutscene1State.lua
+++ Corona Rush_Surviving the 300 seconds/src/states/Cutscene1State.lua
@@ -46,8 +46,6 @@ end
 
 function Cutscene1State:update(dt)
 
-
-
     if next_text == 4 then
         gSounds['news']:stop()
     end
@@ -58,13 +56,12 @@ function Cutscene1State:update(dt)
         gSounds['bgm1']:play()
     end
 
-
     if next_text ~= nil then
         --if variable next_text exists
         if love.keyboard.wasPressed('space') then
         --controls user input to go to forward
             next_text = next_text + 1
-           x = x + 1
+            x = x + 1
 
 
         elseif love.keyboard.wasPressed('x') then
@@ -74,15 +71,11 @@ function Cutscene1State:update(dt)
 
         elseif love.keyboard.wasPressed('s') then
             next_text = 71
-      x = 71
-              gSounds['news']:stop()
+            x = 71
+            gSounds['news']:stop()
         end
 
     end
-
-
-
-
 
     if next_text == 72 then
         gStateMachine:change('char-select')
@@ -96,7 +89,6 @@ function Cutscene1State:update(dt)
         dialog_finished1 = true
     end
 
-
     self:setState()
 
     self.currentAnimation:update(dt)
@@ -104,7 +96,7 @@ end
 
 function Cutscene1State:render()
 
-    love.graphics.draw(gTextures.dialogboxcs, 0, 515, 0, 1, 1)
+    love.graphics.draw(gTextures.dialogboxcs, 0, WINDOW_HEIGHT - 200, 0, 1, 1)
 
     if next_text == 1 or next_text == 2  then
         love.graphics.draw(gTextures.news1, 0, 0, 0, 5, 5)
@@ -112,30 +104,30 @@ function Cutscene1State:render()
 
     if next_text == 3  then
         if dialog_finished == false then
-          if self.animation.news.currentFrame < 4 then
-            love.graphics.draw(gTextures.news, gFrames.news[self.currentAnimation:getCurrentFrame()], 0, 0, 0, 5, 5)
-          end
+            if self.animation.news.currentFrame < 4 then
+                love.graphics.draw(gTextures.news, gFrames.news[self.currentAnimation:getCurrentFrame()], 0, 0, 0, 5, 5)
+            end
         else
-          love.graphics.draw(gTextures.news4, 0, 0, 0, 5, 5)
+            love.graphics.draw(gTextures.news4, 0, 0, 0, 5, 5)
         end
     end
 
     if next_text == 6  then
         if dialog_finished1 == false then
-          if self.animation.eyeopen.currentFrame < 14 then
-            love.graphics.draw(gTextures.eyeopen, gFrames.eyeopen[self.currentAnimation:getCurrentFrame()], 0, 0, 0, 5, 5)
-            love.graphics.draw(gTextures.dialogboxcs, 0, 515, 0, 1, 1)
-          end
+            if self.animation.eyeopen.currentFrame < 14 then
+                love.graphics.draw(gTextures.eyeopen, gFrames.eyeopen[self.currentAnimation:getCurrentFrame()], 0, 0, 0, 5, 5)
+                love.graphics.draw(gTextures.dialogboxcs, 0, WINDOW_HEIGHT - 200, 0, 1, 1)
+            end
         else
-          love.graphics.draw(gTextures.curtain, 0, 0, 0, 5, 5)
-          love.graphics.draw(gTextures.dialogboxcs, 0, 515, 0, 1, 1)
+            love.graphics.draw(gTextures.curtain, 0, 0, 0, 5, 5)
+            love.graphics.draw(gTextures.dialogboxcs, 0, WINDOW_HEIGHT - 200, 0, 1, 1)
         end
     end
 
     if next_text == 7 or next_text == 8 or next_text == 9 or next_text == 10 or next_text == 11 or
     next_text == 12 or next_text == 13 or next_text == 14 then
         love.graphics.draw(gTextures.curtain, 0, 0, 0, 5, 5)
-        love.graphics.draw(gTextures.dialogboxcs, 0, 515, 0, 1, 1)
+        love.graphics.draw(gTextures.dialogboxcs, 0, WINDOW_HEIGHT - 200, 0, 1, 1)
     end
 
     if next_text == 15 or next_text == 16 or next_text == 17 or next_text == 18 or
@@ -143,29 +135,29 @@ function Cutscene1State:render()
     next_text == 24 or next_text == 25 or next_text == 26 then
 
         love.graphics.draw(gTextures.oxygen, gFrames.oxygen[self.currentAnimation:getCurrentFrame()], 0, 0, 0, 5, 5)
-        love.graphics.draw(gTextures.dialogboxcs, 0, 515, 0, 1, 1)
+        love.graphics.draw(gTextures.dialogboxcs, 0, WINDOW_HEIGHT - 200, 0, 1, 1)
     end
 
     if next_text == 29 or next_text == 30 or next_text == 31  then
         love.graphics.draw(gTextures.friends, 0, 0, 0, 5, 5)
-        love.graphics.draw(gTextures.dialogboxcs, 0, 515, 0, 1, 1)
+        love.graphics.draw(gTextures.dialogboxcs, 0, WINDOW_HEIGHT - 200, 0, 1, 1)
     end
 
     if next_text == 37 or next_text == 38 or next_text == 39 or next_text == 40 or
     next_text == 41 or next_text == 42 or next_text == 43 or next_text == 44 or next_text == 45 or next_text == 46 then
 
         love.graphics.draw(gTextures.meet, 0, 0, 0, 5, 5)
-        love.graphics.draw(gTextures.dialogboxcs, 0, 515, 0, 1, 1)
+        love.graphics.draw(gTextures.dialogboxcs, 0, WINDOW_HEIGHT - 200, 0, 1, 1)
     end
 
     if next_text == 47 or next_text == 48 or next_text == 49 then
         love.graphics.draw(gTextures.window, 0, 0, 0, 5, 5)
-        love.graphics.draw(gTextures.dialogboxcs, 0, 515, 0, 1, 1)
+        love.graphics.draw(gTextures.dialogboxcs, 0, WINDOW_HEIGHT - 200, 0, 1, 1)
     end
 
     if next_text == 50 or next_text == 51 or next_text == 52 then
         love.graphics.draw(gTextures.chaos, gFrames.chaos[self.currentAnimation:getCurrentFrame()], 0, 0, 0, 5, 5)
-        love.graphics.draw(gTextures.dialogboxcs, 0, 515, 0, 1, 1)
+        love.graphics.draw(gTextures.dialogboxcs, 0, WINDOW_HEIGHT - 200, 0, 1, 1)
     end
 
     if next_text == 54 or next_text == 55 or next_text == 56 or next_text == 57 or next_text == 58 or next_text == 59
@@ -173,7 +165,7 @@ function Cutscene1State:render()
     65 or next_text == 66 or next_text == 67 or next_text == 68 or next_text == 69 or next_text == 70 or next_text == 71 then
 
         love.graphics.draw(gTextures.meet, 0, 0, 0, 5, 5)
-        love.graphics.draw(gTextures.dialogboxcs, 0, 515, 0, 1, 1)
+        love.graphics.draw(gTextures.dialogboxcs, 0, WINDOW_HEIGHT - 200, 0, 1, 1)
     end
 
 
@@ -182,23 +174,21 @@ function Cutscene1State:render()
             love.graphics.setFont(gFonts.xxsmall)
             love.graphics.setColor(1, 1, 1, 1)
             love.graphics.print(dialogue_Cutscene1[next_text], 150, 500)
-          --  love.graphics.print(next_text, 0, 0)
 
         elseif next_text == x then
             love.graphics.setFont(gFonts.xxsmall)
             love.graphics.setColor(1, 1, 1, 1)
-            love.graphics.print(dialogue_Cutscene1[next_text], 150, 475)
-            --love.graphics.print(next_text, 0, 0)
+            love.graphics.printf(dialogue_Cutscene1[next_text], 100, 475, WINDOW_WIDTH - 170, 'left')
         end
 
     end
-    love.graphics.printf("Press s to skip", 20, 670,
+    love.graphics.printf("Press s to skip", 20, 680,
         WINDOW_WIDTH, 'left')
 
-        love.graphics.printf("Press x for previous", 10, 670,
+        love.graphics.printf("Press x for previous", 10, 680,
             WINDOW_WIDTH, 'center')
 
-    love.graphics.printf("Press Spacebar to continue", -20, 670,
+    love.graphics.printf("Press Spacebar to continue", -20, 680,
         WINDOW_WIDTH, 'right')
 
 
